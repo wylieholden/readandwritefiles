51037cfbee0d74ab5a6bca575eee9c18624d1da1
wylieholden <wylie_holden1@baylor.edu>
2023-01-27 23:45:51 -0600

    start

--- customer_country.py
+++ customer_country.py
@@ -7,12 +7,15 @@ NEWFILE = "customer_country.csv"
 
 def main():
     # open
-    infile = open(CSVFILE, "r", "newline=")
-    outfile = open(NEWFILE, "w", "newline=")
+    infile = open(CSVFILE, "r", newline="")
+    outfile = open(NEWFILE, "w", newline="")
+
     reader = csv.reader(infile)
     writer = csv.writer(outfile, delimiter=",")
     next(reader)
 
+    header = ["First Name", "Last Name", "Country"]
+    writer.writerow(header)
     # loop
     for row in reader:
         fname = row[1]
@@ -20,7 +23,8 @@ def main():
         country = row[4]
 
         # display
-        writer.writerow("Name (first and last):", fname, lname, "Country:", country)
+        new_row = [fname, lname, country]
+        writer.writerow(new_row)
     infile.close()
     outfile.close()
 
--- employee_bonus.py
+++ employee_bonus.py
@@ -19,9 +19,9 @@ def main():
         bonus = row[4]
 
         # display
-        print(
-            "Name (first and last):", fname, lname, "Salary:", salary, "Bonus:", bonus
-        )
+        print("Name (first and last):", fname, lname)
+        print("Salary:", salary)
+        print("Bonus:", bonus)
 
     infile.close()
 
